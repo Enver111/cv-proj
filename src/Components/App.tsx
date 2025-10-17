bbc0e9c337b084c6c404b0c52d244dc240fe0206
Enver <enver.erfanovich@gmail.com>
2025-10-17 23:32:28 +0300

    Mounted about and skills

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,15 @@
+import About from "./About";
 import Header from "./Header";
 import Main from "./Main";
+import Skills from "./Skills";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <About />
+      <Skills />
     </>
   );
 }
