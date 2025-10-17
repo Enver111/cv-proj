5ecc70ae117c8ce35446e6815db406cc55be6a1c
Enver <enver.erfanovich@gmail.com>
2025-10-17 19:13:56 +0300

    Mounted main component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,13 @@
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
