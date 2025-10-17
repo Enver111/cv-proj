ae98cc48a6f30456947841d15c2b6e1bd9b02125
Enver <enver.erfanovich@gmail.com>
2025-10-17 23:32:09 +0300

    Fixed the display of text about me

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,86 +1,49 @@
-import { useState, useEffect } from "react";
-import Button from "../assets/icons/Button.svg";
+import Container from "./Conteiner";
+import Scroll from "./UI/Scroll";
+import Mac from "../assets/bg/mac.png";
 
 export default function About() {
-  const fullText = `
-    <h1 class="text-[64px]"> Hey
-      <br /> I'm <span class="text-[#12F7D6]">Enver</span>,
-      <br /> Frontend Developer
-    </h1>
-    <p class="text-[16px] my-[30px]">
-      I help business grow by crafting amazing web experiences.
-      <br /> If you’re looking for a developer that likes to get stuff done,
-    </p>
-    <div class="flex gap-2 items-center">
-      <p class="text-[32px] text-[#12F7D6]">let’s talk</p>
-      <a href="#"><img src=${Button} alt="Button" /></a>
-    </div>
-  `;
-
-  const [displayed, setDisplayed] = useState("");
-  const [visible, setVisible] = useState(false);
-
-  const typingSpeed = 50; // скорость печати (мс)
-  const repeatDelay = 50000000; // пауза перед повтором (мс)
-
-  useEffect(() => {
-    const tokens = fullText.split(/(<[^>]+>)/g).filter(Boolean);
-
-    let tokenIndex = 0;
-    let charIndex = 0;
-    let currentText = "";
-
-    function startTyping() {
-      const interval = setInterval(() => {
-        const token = tokens[tokenIndex];
-
-        if (!token) {
-          clearInterval(interval);
-          // ждём и запускаем печать заново
-          setTimeout(() => {
-            setDisplayed("");
-            setVisible(false);
-            tokenIndex = 0;
-            charIndex = 0;
-            currentText = "";
-            startTyping();
-          }, repeatDelay);
-          return;
-        }
-
-        if (token.startsWith("<")) {
-          currentText += token;
-          setDisplayed(currentText);
-          setVisible(true);
-          tokenIndex++;
-          charIndex = 0;
-        } else {
-          currentText += token[charIndex];
-          setDisplayed(currentText);
-          setVisible(true);
-          charIndex++;
-          if (charIndex >= token.length) {
-            tokenIndex++;
-            charIndex = 0;
-          }
-        }
-      }, typingSpeed);
-    }
-
-    startTyping();
-
-    return () => setDisplayed(""); // очистка при размонтировании
-  }, [typingSpeed]);
-
   return (
-    <div
-      className="text-white font-mono mx-20"
-      style={{
-        opacity: visible ? 1 : 0,
-        transform: visible ? "translateY(0px)" : "translateY(5px)",
-        transition: "opacity 0.3s ease, transform 0.3s ease",
-      }}
-      dangerouslySetInnerHTML={{ __html: displayed }}
-    />
+    <div className="bg-[#1A1E23]">
+      <div className="bg-[url(./assets/bg/about-me.png)]">
+        <Container className="pb-[128px]">
+          <Scroll />
+          <div className="flex justify-between items-center mt-[120px]">
+            <div>
+              <h1 className="w-[367px] text-[64px] bg-[#1A1E23] rounded-tl-[40px] rounded-br-[40px] border-[4px] text-center border-[#12F7D6] mb-[60px]">
+                About Me
+              </h1>
+              <div className="px-[40px] py-[58px] rounded-[40px] bg-[#292F36] w-[880px]">
+                <h2 className="text-[32px] text-[#12F7D6]">Hello!</h2>
+                <br />
+                <p className="text-[16px]">
+                  My name is Sinan and I specialize in web developement that
+                  utilizes HTML, CSS, JS, and REACT etc.
+                </p>
+                <br />
+                <p>
+                  I am a highly motivated individual and eternal optimist
+                  dedicated to writing clear, concise, robust code that works.
+                  Striving to never stop learning and improving.
+                </p>
+                <br />
+                <p>
+                  When I'm not coding, I am writing bolgs, reading, or picking
+                  up some new hands-on art project like photography.
+                </p>
+                <br />
+                <p>
+                  I like to have my perspective and belief systems challenged so
+                  that I see the world through new eyes.
+                </p>
+              </div>
+            </div>
+            <div>
+              <img src={Mac} alt="Mac" />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
   );
 }
